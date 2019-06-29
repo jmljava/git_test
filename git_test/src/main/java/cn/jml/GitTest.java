fb4a9da249279571d3edb018c9f8e233f7e73d0c
zhangshan <zhangshan@qq.com>
2019-06-29 09:30:51 +0800

    git_test添加到git

---
+++ git_test/src/main/java/cn/jml/GitTest.java
@@ -0,0 +1,12 @@
+package cn.jml;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+
+
+    }
+}
